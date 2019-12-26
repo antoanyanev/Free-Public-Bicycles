6b6f2269168f1e86c13d67b3851608f1f5764aab
antoayanev <antoan.yanev5@gmail.com>
2019-12-26 17:00:35 +0200

    Sending GPS information

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,49 +1,48 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-#include <LoRaLib.h>
+#include <SPI.h>
+#include <LoRa.h>
 
 #define RXPIN 9
 #define TXPIN 8
 
-TinyGPSPlus gps;
-SX1276 lora = new LoRa(6, 7, 4);
-SoftwareSerial gpsSerial(RXPIN, TXPIN);
+#define INTERVAL 5
 
-char _latitude[16];
-char _longitude[16];
-char temp[16];
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(RXPIN, TXPIN);
 
 uint8_t _day;
 uint8_t _month;
-int _year;
-
 uint8_t _hour;
 uint8_t _minute;
 uint8_t _second;
 
+int _year;
+int iterations = 0;
 int read_data_count = 0;
 
+char _latitude[16];
+char _longitude[16];
+char temp[16];
 char _packet[64];
-int state;
 
 void get_info();
 void create_packet();
+void calculate_iterations();
 
 void setup() {
   Serial.begin(9600);
-  gpsSerial.begin(9600);
+  while (!Serial);
 
-  state = lora.begin(868, 125, 9, 7, 0x12, 17, 100, 8, 0);
+  LoRa.setPins(10, 7, 2);
 
-  if (state == ERR_NONE) {
-    Serial.println("Success!");
-  } else {
-    while (true) { }
+  if (!LoRa.begin(868E6)) {
+    Serial.println("Startling LoRa failed!");
+    while (1);
   }
 
-  digitalWrite(7, HIGH);
-  delay(10);
-  digitalWrite(7, LOW);
+  calculate_iterations();
+  gpsSerial.begin(9600);
 }
 
 void loop() {
@@ -53,24 +52,18 @@ void loop() {
       read_data_count++;
     }
   }
-    
+
   if (millis() > 5000 && gps.charsProcessed() < 10) {
     Serial.println("No GPS detected");
     while (true) { }
   }
 
-  if (read_data_count == 8) {
+  if (read_data_count == 40) {
     create_packet();
-    state = lora.transmit(_packet);
-
-    if (state == ERR_NONE) {
-      Serial.println("Successfully sent data.");
-    } else if (state == ERR_PACKET_TOO_LONG) {
-      Serial.println("Packet is too long.");
-    } else if (state == ERR_TX_TIMEOUT) {
-      Serial.println("Timeout!");
-    }
-
+    Serial.println(_packet);
+    LoRa.beginPacket();
+    LoRa.print(_packet);
+    LoRa.endPacket();
     read_data_count = 0;
   }
 }
@@ -95,6 +88,11 @@ void get_info() {
     _hour = gps.time.hour();
   }
 }
+
 void create_packet() {
   sprintf(_packet, "%s, %s, %d/%d/%d, %d:%d:%d", _latitude, _longitude, _day, _month, _year, _hour, _minute, _second);
+}
+
+void calculate_iterations() {
+  iterations = INTERVAL * 8;
 }
