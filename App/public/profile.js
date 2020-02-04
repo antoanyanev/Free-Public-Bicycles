92d0ca69753a1af9955f7a894928c96a305f411b
antoanyanev <antoan.yanev5@gmail.com>
2020-02-05 00:59:43 +0200

    Refactored map.js & profile.js, added map styles file styles.js

--- App/public/profile.js
+++ App/public/profile.js
@@ -30,31 +30,8 @@ $(document).ready(() => { // Wait for page to load
 
     $.get('https://www.freepublicbicycles.org/user/trips/all', (data) => { // Get all trips of user
         trips = data;
-
-        let j = 0;
-        for (let i = 0; i < data.length; i++) {
-            if (id != data[i].id) {
-                j++;
-                $("#trips").append(`<li id="${data[i].id}">Trip Number: ${j}</li>`); // Add new trip button
-                id = data[i].id;
-            }
-        }
-
-        id = 0;
-        for (let j = 0; j < data.length; j++) {
-            if (id != data[j].id) {
-                $(`#${data[j].id}`).click((event) => { // Add click event listener for each button
-                    path = [];
-                    tripLocations = [];
-                    tripIDs = [];
-                    currentID = parseInt(event.target.id);
-
-                    setTripIDs();
-                    setTripLocations();
-                    createMarkers();
-                });
-            }
-        } 
+        setTrips(trips);
+        setEventListeners(trips);
     });
 });
 
@@ -69,86 +46,7 @@ function initMap() { // Google maps API map setup
     map = new google.maps.Map(document.getElementById('map'), { // Create map object
         center: {lat: 42.7254843, lng: 23.3056195}, // Set center point coordinates
         zoom: 16, // Set the initial zoom level of the map
-        styles: [ // Set map styles
-          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-          {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [{color: '#263c3f'}]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#6b9a76'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [{color: '#38414e'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry.stroke',
-            stylers: [{color: '#212a37'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#9ca5b3'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [{color: '#746855'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{color: '#1f2835'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#f3d19c'}]
-          },
-          {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [{color: '#2f3948'}]
-          },
-          {
-            featureType: 'transit.station',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [{color: '#17263c'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#515c6d'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [{color: '#17263c'}]
-          }
-        ]
+        styles: styles
     });
 }
 
@@ -191,4 +89,33 @@ function createMarkers() { // Create start marker, end marker and trace in betwe
         strokeWeight: 2,
         map: map
     });
+}
+
+function setTrips(data) { // Set HTML trips list items proeprties
+    let j = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (id != data[i].id) {
+            j++;
+            $("#trips").append(`<li id="${data[i].id}">Trip Number: ${j}</li>`); // Add new trip button
+            id = data[i].id;
+        }
+    }
+}
+
+function setEventListeners(data) { // Set HTML trips list items mouse click event listeners
+    id = 0;
+    for (let j = 0; j < data.length; j++) {
+        if (id != data[j].id) {
+            $(`#${data[j].id}`).click((event) => { // Add click event listener for each button
+                path = [];
+                tripLocations = [];
+                tripIDs = [];
+                currentID = parseInt(event.target.id);
+
+                setTripIDs();
+                setTripLocations();
+                createMarkers();
+            });
+        }
+    }
 }
